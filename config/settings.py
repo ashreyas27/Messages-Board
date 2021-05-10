640fc6eacf4365117da7418eb0ef5f042e9beb3e
Shreyas <shreyasusa27@gmail.com>
2021-05-10 12:31:02 +0530

    Deployed in Heroku

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'vbz*v*(bha__fw!-5)v=3@crd5z_*on!5fsvvit=wh+kp-^knm'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.herokuapp.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
